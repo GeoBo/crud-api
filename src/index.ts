d72591f2e855a13c55f5bd2b9c41e62ce01f29dd
Geobo <bozhys@gmail.com>
2022-12-15 23:29:46 +0300

    feat: add webpack, nodemon, typescript plugins

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// dev-server.js
+// const express = require('express');
+// const app = express();
+// // Import routes
+// require('./_routes')(app);
+// app.set('port', 8080);
+// app.listen(app.get('port'), function () {
+//     console.log('Node App Started');
+// });
+console.log('Node App Started');
+console.log('Already!!!');
+console.log('Already!!!');
